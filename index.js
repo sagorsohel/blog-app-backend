b551526554a74fc81d0b0bf9aa3e5373a4e5abc0
sagorsohel <sohel19sagor@gmail.com>
2025-05-17 20:47:00 +0600

    Comment out unused middleware in index.js

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoute);
+// app.use((req, res, next) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
